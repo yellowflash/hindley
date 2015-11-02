75e4fabfa6ba79abb21700119461d02a36e549f6
Minato Namikaze <minato.yelloflash@gmail.com>
2015-11-02 15:12:59 +0530

    Defining free variables

--- src/main/scala/hindley/Expression.scala
+++ src/main/scala/hindley/Expression.scala
@@ -1,8 +1,16 @@
 package hindley
 
 
-sealed trait Expression
+sealed trait Expression {
+  def freeVariables:Set[TermVariable]
+}
 
-case class TermVariable(name:String) extends Expression
-case class Abstraction(param: TermVariable, exp: Expression) extends Expression
-case class Application(fn: Expression, arg: Expression) extends Expression
+case class TermVariable(name:String) extends Expression {
+  override def freeVariables: Set[TermVariable] = Set(this)
+}
+case class Abstraction(param: TermVariable, exp: Expression) extends Expression {
+  override def freeVariables: Set[TermVariable] = exp.freeVariables - param
+}
+case class Application(fn: Expression, arg: Expression) extends Expression {
+  override def freeVariables: Set[TermVariable] = fn.freeVariables ++ arg.freeVariables
+}
